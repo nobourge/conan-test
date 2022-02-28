a3692f177b0a5517fea0c29addc4a5818a0f4fa4
nobourge <57195536+nobourge@users.noreply.github.com>
2022-02-28 19:30:07 +0100

    cmakelists

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by bourg on 27-02-22.
+//
+
+#include "Poco/MD5Engine.h"
+#include "Poco/DigestStream.h"
+#include <iostream>
+int main(int argc, char** argv)
+{
+  Poco::MD5Engine md5;
+  Poco::DigestOutputStream ds(md5);
+  ds << "abcdefghijklmnopqrstuvwxyz";
+  ds.close();
+  std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+  return 0;
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+//
+// Created by bourg on 27-02-22.
+//
+
+#ifndef CONAN_TEST__MAIN_H_
+#define CONAN_TEST__MAIN_H_
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+#endif //CONAN_TEST__MAIN_H_
